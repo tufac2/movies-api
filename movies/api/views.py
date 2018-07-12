921cd50eb0a38f3442fa27c54c752fa19339bfb9
Fabian Almenara <tufac2@gmail.com>
2018-07-12 11:31:45 +0200

    Heroku

--- movies/api/views.py
+++ movies/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+from api.models import Movie
+from api.serializers import MovieSerializer
+
+
+class MovieCreateView(ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class MovieDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
