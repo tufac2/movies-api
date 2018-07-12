7f1b91a78f0e4b9dd3fde4b7eb2d736671e94f64
Fabian Almenara <tufac2@gmail.com>
2018-07-12 11:51:19 +0200

    Heroku insettings

--- movies/movies/settings.py
+++ movies/movies/settings.py
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ALLOWED_HOSTS = ['movie-api-staging-fabian.herokuapp.com', 'movie-api-production-fabian.herokuapp.com']
